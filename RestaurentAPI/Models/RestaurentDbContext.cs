b99485fb957822de563de5a0b38426ba5a3fe343
Saddam Hossen <saddam.itsd@goldenharvestbd.com>
2021-05-27 17:40:59 +0600

    first commit

---
+++ RestaurentAPI/Models/RestaurentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurentAPI.Models
+{
+    public class RestaurentDbContext:DbContext  
+    {
+        public RestaurentDbContext(DbContextOptions<RestaurentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+
+        public DbSet<OrderMaster> OrderMasters { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    }
+
+}
